33893da283f19076a6214ba2531772f84169e22f
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2025-02-24 16:46:53 -0500

    Add param and throws tags to String extensions

--- libraries/stdlib/samples/test/samples/text/strings.kt
+++ libraries/stdlib/samples/test/samples/text/strings.kt
@@ -724,6 +724,7 @@ class Strings {
         assertTrue(str.startsWith("abc"))
         assertFalse(str.startsWith("ABC"))
         assertFalse(str.startsWith("bcd"))
+        assertFalse(str.startsWith("abcdef"))
     }
 
     @Sample
@@ -756,6 +757,7 @@ class Strings {
         assertTrue(str.endsWith("cde"))
         assertFalse(str.endsWith("CDE"))
         assertFalse(str.endsWith("bcd"))
+        assertFalse(str.endsWith("_abcde"))
     }
 
     @Sample
