744c89327b73be5c50e4a5263e141de46c92df50
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2025-04-25 12:19:38 +0200

    [Lexer] Implement KMP Kotlin lexer (written in Kotlin)
    Implement generation and testing infrastructure around it
    
    ^KT-77048 Fixed

--- compiler/kotlin-syntax/build.gradle.kts
+++ compiler/kotlin-syntax/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import java.net.URI
 
 plugins {
     kotlin("multiplatform")
@@ -27,10 +28,69 @@ kotlin {
                 srcDir("common/src")
             }
         }
-        val jvmMain by getting {
+        val jvmTest by getting {
+            dependencies {
+                implementation(project(":compiler:psi"))
+                implementation(commonDependency("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm"))
+                implementation(intellijCore())
+                runtimeOnly(libs.intellij.fastutil)
+                implementation(project(":compiler:test-infrastructure-utils"))
+                implementation(libs.junit.jupiter.api)
+                runtimeOnly(libs.junit.jupiter.engine)
+                api(kotlinTest("junit"))
+            }
             kotlin {
-                srcDir("jvm/src")
+                srcDir("jvm/test")
             }
         }
     }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+val flexGeneratorClasspath: Configuration by configurations.creating
+
+dependencies {
+    flexGeneratorClasspath(commonDependency("org.jetbrains.intellij.deps.jflex", "jflex")) {
+        // Flex brings many unrelated dependencies, so we are dropping them because only a flex `.jar` file is needed.
+        // It can be probably removed when https://github.com/JetBrains/intellij-deps-jflex/issues/10 is fixed.
+        isTransitive = false
+    }
+}
+
+tasks.register<JavaExec>("generateKotlinLexer") {
+    mainClass = "jflex.Main"
+    classpath = files(flexGeneratorClasspath)
+
+    val lexerDir = projectDir.resolve("common/src/org/jetbrains/kotlin/kmp/lexer")
+
+    // TODO: get rid of the skeleton or implement its caching
+    // It's blocked by https://github.com/JetBrains/intellij-deps-jflex/issues/9
+    // Currently it's forced to use the latest skeleton version from master with the assumption that the latest flex version is used.
+    val skeletonFile = layout.buildDirectory.file("idea-flex-kotlin.skeleton").get().asFile
+
+    doFirst {
+        val skeletonUrl = "https://raw.githubusercontent.com/JetBrains/intellij-community/master/tools/lexer/idea-flex-kotlin.skeleton"
+        println("Downloading skeleton file $skeletonUrl")
+        URI.create(skeletonUrl).toURL().openStream().use { input ->
+            skeletonFile.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+    }
+
+    argumentProviders.add(CommandLineArgumentProvider {
+        listOf(
+            lexerDir.resolve("Kotlin.flex").absolutePath,
+            "-skel",
+            skeletonFile.absolutePath,
+            "-d",
+            lexerDir.absolutePath,
+            "--output-mode",
+            "kotlin",
+            "--nobak", // Prevent generating backup `.kt~` files
+        )
+    })
 }
---
+++ compiler/kotlin-syntax/jvm/test/infra/TestDataUtils.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package infra
+
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+
+object TestDataUtils {
+    val testDataDir: Path = Paths.get(File(System.getProperty("user.dir") ?: ".").parent, "testData")
+
+    // TODO: for some reason, it's not possible to depend on `:compiler:test-infrastructure-utils` here
+    // See org.jetbrains.kotlin.codeMetaInfo.CodeMetaInfoParser
+    private val openingDiagnosticRegex = """(<!([^"]*?((".*?")(, ".*?")*?)?[^"]*?)!>)""".toRegex()
+    private val closingDiagnosticRegex = """(<!>)""".toRegex()
+
+    private val xmlLikeTagsRegex = """(</?(?:selection|expr|caret)>)""".toRegex()
+
+    private val allMetadataRegex =
+        """(${closingDiagnosticRegex.pattern}|${openingDiagnosticRegex.pattern}|${xmlLikeTagsRegex.pattern})""".toRegex()
+
+    fun checkKotlinFiles(kotlinFileChecker: (String, Path) -> Unit) {
+        testDataDir.toFile().walkTopDown().filter { it.isFile && it.extension.let { ext -> ext == "kt" || ext == "kts" || ext == "nkt" } }
+            .forEach {
+                val text = it.readText()
+                val refinedText = text.replace(allMetadataRegex, "")
+                kotlinFileChecker(refinedText, it.toPath())
+            }
+    }
+}
+
