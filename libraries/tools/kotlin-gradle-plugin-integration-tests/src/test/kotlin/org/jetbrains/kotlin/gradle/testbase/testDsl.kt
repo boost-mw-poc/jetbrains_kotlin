JetBrains/kotlin

[Gradle] Use injections for XCFramework Resources IT
^KT-77165

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/testDsl.kt
@@ -573,9 +573,9 @@ class TestProject(
         }
     }
 
-    fun copyOtherProjectDir(otherProjectName: String) {
+    fun copyOtherProjectDir(otherProjectName: String, destination: String = otherProjectName) {
         val otherProjectPath = otherProjectName.testProjectPath
-        otherProjectPath.copyRecursively(projectPath.resolve(otherProjectName))
+        otherProjectPath.copyRecursively(projectPath.resolve(destination))
     }
 }
 

boost-mw-poc/jetbrains_kotlin

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/testDsl.kt
@@ -579,9 +579,9 @@ class TestProject(
      *
      * @param otherProjectName The name of the other project whose directory is copied from its test data.
      */
-    fun embedDirectoryFromTestData(otherProjectName: String) {
+    fun embedDirectoryFromTestData(otherProjectName: String, destination: String = otherProjectName) {
         val otherProjectPath = otherProjectName.testProjectPath
-        otherProjectPath.copyRecursively(projectPath.resolve(otherProjectName))
+        otherProjectPath.copyRecursively(projectPath.resolve(destination))
     }
 }
 